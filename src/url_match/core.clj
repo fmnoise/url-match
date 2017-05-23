e2e2650f14a9d684e4cc3c52dfc727c96d632d24
fmnoise@gmail.com <fmnoise@gmail.com>
2017-05-23 12:38:37 +0300

    use positive lookahead for query, coerse int values, sort by key

--- src/url_match/core.clj
+++ src/url_match/core.clj
@@ -17,12 +17,13 @@
 (defn- make-query-re
   [query]
   (when-not (empty? query)
-    (str "\\?.*"
-      (s/join "&"
-        (map #(str "(?:" % ")")
-          (map #(s/replace % param "(\\\\w+)") query))))))
+    (->> query
+         (map #(s/replace % param "(\\\\w+)"))
+         (map #(str "(?=.*" % ")"))
+         s/join
+         (#(str "\\?.*" % ".+")))))
 
-(defn- make-regex
+(defn- make-pattern-regex
   [pattern]
   (re-pattern
     (str
@@ -36,13 +37,24 @@
     (map (comp keyword last) (re-seq param (:path pattern)))
     (map (comp keyword last #(s/split % #"=\?")) (:query pattern))))
 
+(defn- try-int
+  [val]
+  (if-let [int-value (re-find #"^[1-9]\d*$" val)]
+    (read-string int-value)
+    val))
+
+
 (defn new-pattern
   [template]
   {:host  (last (re-find host template))
    :path  (last (re-find path template))
    :query (map last (re-seq query template))})
 
+; TODO: use named regexp groups instead of zipmap
 (defn recognize
   [pattern url]
-  (when-let [vals (re-seq (make-regex pattern) url)]
-    (into [] (zipmap (extract-keys pattern) (rest (first vals))))))
+  (when-let [values (re-seq (make-pattern-regex pattern) url)]
+    (->> (zipmap (extract-keys pattern) (rest (first values)))
+         (into [])
+         (map (fn [[k v]] [k (try-int v)]))
+         sort)))
--- test/url_match/core_test.clj
+++ test/url_match/core_test.clj
@@ -4,16 +4,16 @@
 
 (def twitter (new-pattern "host(twitter.com); path(?user/status/?id);"))
 (def dribbble (new-pattern "host(dribbble.com); path(shots/?id); queryparam(offset=?offset);"))
-(def dribbble2 (new-pattern "host(dribbble.com); path(shots/?id); queryparam(offset=?offset); queryparam(list=?type);"))
+(def dribbble2 (new-pattern "host(dribbble.com); path(shots/?id); queryparam(list=?type); queryparam(offset=?offset);"))
 
 (def twitter-url "http://twitter.com/bradfitz/status/562360748727611392")
 (def dribbble-url "https://dribbble.com/shots/1905065-Travel-Icons-pack?offset=1&list=users")
 (def bad-dribbble-url "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users")
 (def bad-twitter-url "https://twitter.com/shots/1905065-Travel-Icons-pack?list=users&offset=1")
 
-(def twitter-params [[:user "bradfitz"] [:id "562360748727611392"]])
-(def dribbble-params [[:id "1905065-Travel-Icons-pack"] [:offset "1"]])
-(def dribbble2-params [[:id "1905065-Travel-Icons-pack"] [:offset "1"] [:type "users"]])
+(def twitter-params [[:id 562360748727611392] [:user "bradfitz"]])
+(def dribbble-params [[:id "1905065-Travel-Icons-pack"] [:offset 1]])
+(def dribbble2-params [[:id "1905065-Travel-Icons-pack"] [:offset 1] [:type "users"]])
 
 (deftest valid-twitter-url
   (is (= twitter-params (recognize twitter twitter-url))))
