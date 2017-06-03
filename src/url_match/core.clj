46699f54017965c11c3689fceb691564157f242a
fmnoise@gmail.com <fmnoise@gmail.com>
2017-06-03 16:39:46 +0300

    fix null pointer exception

--- src/url_match/core.clj
+++ src/url_match/core.clj
@@ -1,5 +1,5 @@
 (ns url-match.core
-  [:require [clojure.string :as s :only [join replace split]]])
+  (:require [clojure.string :as s :only [join replace split]]))
 
 (def ^:private host #"(host)\((.+?)\);")
 (def ^:private path #"(path)\((.+?)\);")
@@ -8,11 +8,13 @@
 
 (defn- make-host-re
   [host]
-  (str ".+://" (s/replace host "." "\\.") "/"))
+  (when-not (empty? host)
+    (str ".+://" (s/replace host "." "\\.") "/")))
 
 (defn- make-path-re
   [path]
-  (s/replace path param "(.+)"))
+  (when-not (empty? path)
+    (s/replace path param "(.+)")))
 
 (defn- make-query-re
   [query]
@@ -31,17 +33,35 @@
       (make-path-re (:path pattern))
       (make-query-re (:query pattern)))))
 
+(defn- path-keys
+  [pattern]
+  (when-let [path (:path pattern)]
+    (map (comp keyword last) (re-seq param path))))
+
+(defn- query-keys
+  [pattern]
+  (when-let [query (:query pattern)]
+    (map (comp keyword last #(s/split % #"=\?")) query)))
+
 (defn- extract-keys
   [pattern]
   (concat
-    (map (comp keyword last) (re-seq param (:path pattern)))
-    (map (comp keyword last #(s/split % #"=\?")) (:query pattern))))
+    (path-keys pattern)
+    (query-keys pattern)))
 
 (defn- try-int
   [val]
   (if-let [int-value (re-find #"^[1-9]\d*$" val)]
     (read-string int-value)
     val))
+
+(defn- recognize-pattern
+  [pattern url]
+  (when-let [values (re-seq (make-pattern-regex pattern) url)]
+    (->> (zipmap (extract-keys pattern) (rest (first values)))
+         (into [])
+         (map (fn [[k v]] [k (try-int v)]))
+         sort)))
 
 (defn new-pattern
   [template]
@@ -52,8 +72,6 @@
 ; TODO: use named regexp groups instead of zipmap
 (defn recognize
   [pattern url]
-  (when-let [values (re-seq (make-pattern-regex pattern) url)]
-    (->> (zipmap (extract-keys pattern) (rest (first values)))
-         (into [])
-         (map (fn [[k v]] [k (try-int v)]))
-         sort)))
+  (let [urls-parts (recognize-pattern pattern url)]
+    (when-not (empty? urls-parts)
+      urls-parts)))
--- test/url_match/core_test.clj
+++ test/url_match/core_test.clj
@@ -5,11 +5,13 @@
 (def twitter (new-pattern "host(twitter.com); path(?user/status/?id);"))
 (def dribbble (new-pattern "host(dribbble.com); path(shots/?id); queryparam(offset=?offset);"))
 (def dribbble2 (new-pattern "host(dribbble.com); path(shots/?id); queryparam(list=?type); queryparam(offset=?offset);"))
+(def empty (new-pattern ""))
 
 (def twitter-url "http://twitter.com/bradfitz/status/562360748727611392")
 (def dribbble-url "https://dribbble.com/shots/1905065-Travel-Icons-pack?offset=1&list=users")
 (def bad-dribbble-url "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users")
 (def bad-twitter-url "https://twitter.com/shots/1905065-Travel-Icons-pack?list=users&offset=1")
+(def empty-url "")
 
 (def twitter-params [[:id 562360748727611392] [:user "bradfitz"]])
 (def dribbble-params [[:id "1905065-Travel-Icons-pack"] [:offset 1]])
@@ -29,3 +31,9 @@
 
 (deftest pattern-with-2-params
   (is (= dribbble2-params (recognize dribbble2 dribbble-url))))
+
+(deftest empty-pattern
+  (is (= nil (recognize empty dribbble-url))))
+
+(deftest empty-pattern-with-empty-url
+  (is (= nil (recognize empty empty-url))))
